896912a0669d4db1175df3cb9764dc43aa464a95
Mary <azade.s.it@gmail.com>
2023-04-14 08:46:34 +1000

    implement App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import { Header } from "./components";
+import ProgressBarList from "./components/ProgressBarList";
 
 function App() {
+  const [progressBars] = useState<{ name: string; defaultValue: number }[]>([
+    { name: "Progress1", defaultValue: 50 },
+    { name: "Progress2", defaultValue: 80 },
+    { name: "Progress3", defaultValue: 10 },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <ProgressBarList progressBars={progressBars} />
     </div>
   );
 }
