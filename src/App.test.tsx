2e5dee5a980c065fecdc1fa786600b4b88b27976
Mary <azade.s.it@gmail.com>
2023-04-14 08:46:45 +1000

    Implement tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Progress Bars", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Progress Bars Demo/i);
   expect(linkElement).toBeInTheDocument();
 });
